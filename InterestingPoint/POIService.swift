e42b7fe96c5d7cb84c76ec8f636734e5dcf15dc2
Jeffrey Fulton <jeffrey.fulton@me.com>
2016-07-22 10:39:13 -0500

    Update POI information to reflect expected sort order.

--- InterestingPoint/POIService.swift
+++ InterestingPoint/POIService.swift
@@ -14,40 +14,40 @@ class POIService {
     lazy var pointsOfInterest: Array<POI> = {
         return [
             POI(
-                title: "First interesting place.",
-                subtitle: "This place is great",
+                title: "First (Nearest)",
+                subtitle: "1st row in tableview",
                 coordinate: CLLocationCoordinate2D(
                     latitude: 49.8519574378154,
                     longitude: -97.2117918551222
                 )
             ),
             POI(
-                title: "Second interesting place.",
-                subtitle: "This place is alright",
+                title: "Second",
+                subtitle: "??? row in tableview",
                 coordinate: CLLocationCoordinate2D(
                     latitude: 49.9040656356851,
                     longitude: -97.1168358331907
                 )
             ),
             POI(
-                title: "Third interesting place.",
-                subtitle: "This place sucks",
+                title: "Third (Farthest)",
+                subtitle: "Last row in tableview",
                 coordinate: CLLocationCoordinate2D(
                     latitude: 49.9508672072522,
                     longitude: -97.2422074558971
                 )
             ),
             POI(
-                title: "Fourth interesting place.",
-                subtitle: "Have I been here before?",
+                title: "Fourth",
+                subtitle: "??? row in tableview",
                 coordinate: CLLocationCoordinate2D(
                     latitude: 49.8716259581715,
                     longitude: -97.0682061864028
                 )
             ),
             POI(
-                title: "Fifth interesting place.",
-                subtitle: "I don't know where I am...",
+                title: "Fifth",
+                subtitle: "??? row in tableview",
                 coordinate: CLLocationCoordinate2D(
                     latitude: 49.8141108489216,
                     longitude: -97.1298990909147
